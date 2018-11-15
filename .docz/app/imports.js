55ff62bd911d221ee62ebe926a7084f5874b55d0
Manjula Dube <dube.manjula668@gmail.com>
2018-11-15 23:22:03 +0100

    add architecture

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,6 +1,8 @@
 export const imports = {
   '1-home.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "1-home" */ '1-home.mdx'),
+  '1.1-architecture.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "1-1-architecture" */ '1.1-architecture.mdx'),
   '2-installation.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "2-installation" */ '2-installation.mdx'),
   '3-modules.mdx': () =>
