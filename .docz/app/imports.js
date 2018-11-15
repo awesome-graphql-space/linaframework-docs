d1d992247a256e8411157f4f5d04abc6dd84adba
Manjula Dube <dube.manjula668@gmail.com>
2018-11-15 22:39:28 +0100

    commit installation

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,4 +1,6 @@
 export const imports = {
   'home.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "home" */ 'home.mdx'),
+  'installation.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "installation" */ 'installation.mdx'),
 }
