8fda1693438027272647da6a6bbf92fd930a03ee
Manjula Dube <dube.manjula668@gmail.com>
2018-11-15 23:09:59 +0100

    add modules and authentication as heading

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,6 +1,10 @@
 export const imports = {
+  'authentication.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "authentication" */ 'authentication.mdx'),
   'home.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "home" */ 'home.mdx'),
   'installation.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "installation" */ 'installation.mdx'),
+  'modules.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "modules" */ 'modules.mdx'),
 }
