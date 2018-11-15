d7a0655809c841c1a8cebb33257bacf5b30e7f56
Manjula Dube <dube.manjula668@gmail.com>
2018-11-15 22:31:05 +0100

    yello

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,12 +1,4 @@
 export const imports = {
-  '2-styles.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "2-styles" */ '2-styles.mdx'),
-  '5-build-next-favorite.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "5-build-next-favorite" */ '5-build-next-favorite.mdx'),
-  '6-add-favorites.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "6-add-favorites" */ '6-add-favorites.mdx'),
-  '7-refactor-render.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "7-refactor-render" */ '7-refactor-render.mdx'),
   'home.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "home" */ 'home.mdx'),
 }
